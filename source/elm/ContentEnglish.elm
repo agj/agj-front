a4943d7d24e82391f5d447bbb10c7acffe157f5a
agj <ale@agj.cl>
2022-11-27 16:56:32 -0300

    Make text content editing easier

--- source/elm/ContentEnglish.elm
+++ source/elm/ContentEnglish.elm
@@ -8,9 +8,21 @@ contentEnglish =
     { title = "Ale Grilli’s website"
     , intro =
         intro
-            "Hello. This is my, **Ale Grilli** (a.k.a. **agj**)’s home on the web. I’m a Chile-based «cross-media creator»\u{200B}—\u{200B}I program interactive media, design visuals, author videos, and more. I'm also a languages nerd. You may contact me via “ale¶agj.cl” (¶→@)."
+            ("Hello. This is my, **Ale Grilli** (a.k.a. **agj**)’s home on the web. "
+                ++ "I’m a Chile-based «cross-media creator»\u{200B}—\u{200B}"
+                ++ "I program interactive media, design visuals, author videos, and more. "
+                ++ "I'm also a languages nerd. You may contact me via “ale¶agj.cl” (¶→@)."
+            )
     , menu = menu "Portfolio" "Blog"
     , links =
         links
-            "I also have a [**pictures log**][piclog]. I run a Japanese-Spanish bilingual Twitter account, [**Japoñol**][japonol], and sparingly tweet from my [main account][twitter]. My **video works** are on [Vimeo][vimeo]. Some of my **code** is up on [Github][github]. I have a page with [my old **games**][games]. [Greasy Fork][greasyfork] houses my **userscripts**. My [Tumblr][tumblr] has some stuff I've found around the web."
+            ("I also have a [**pictures log**][piclog]. "
+                ++ "I run a Japanese-Spanish bilingual Twitter account, [**Japoñol**][japonol], "
+                ++ "and sparingly tweet from my [main account][twitter]. "
+                ++ "My **video works** are on [Vimeo][vimeo]. "
+                ++ "Some of my **code** is up on [Github][github]. "
+                ++ "I have a page with [my old **games**][games]. "
+                ++ "[Greasy Fork][greasyfork] houses my **userscripts**. "
+                ++ "My [Tumblr][tumblr] has some stuff I've found around the web."
+            )
     }
--- source/elm/ContentSpanish.elm
+++ source/elm/ContentSpanish.elm
@@ -8,9 +8,22 @@ contentSpanish =
     { title = "Web de Ale Grilli"
     , intro =
         intro
-            "Hola. Este es el sitio web de **Ale Grilli** (a veces **agj**). Soy un «creador transmedial»\u{200B}—\u{200B}desde Chile programo medios interactivos, diseño gráfica, creo videos, y otras cosas. Me gustan mucho los idiomas. Me puedes contactar por medio de “ale¶agj.cl” (¶→@)."
+            ("Hola. Este es el sitio web de **Ale Grilli** (a veces **agj**). "
+                ++ "Soy un «creador transmedial»\u{200B}—\u{200B}"
+                ++ "desde Chile programo medios interactivos, diseño gráfica, creo videos, y otras cosas. "
+                ++ "Me gustan mucho los idiomas. "
+                ++ "Me puedes contactar por medio de “ale¶agj.cl” (¶→@)."
+            )
     , menu = menu "Portafolio" "Blog"
     , links =
         links
-            "También tengo un [**archivo de imágenes**][piclog]. Tuiteo en japonés y español en [**Japoñol**][japonol], y a veces en mi [cuenta principal][twitter]. Mis **videos** están en [Vimeo][vimeo]. Parte de mi **código** está en [Github][github]. Una página alberga [mis **videojuegos** viejos][games]. En [Greasy Fork][greasyfork] están mis **userscripts**. En mi [Tumblr][tumblr] tengo cosas raras de la web."
+            ("También tengo un [**archivo de imágenes**][piclog]. "
+                ++ "Tuiteo en japonés y español en [**Japoñol**][japonol], "
+                ++ "y a veces en mi [cuenta principal][twitter]. "
+                ++ "Mis **videos** están en [Vimeo][vimeo]. "
+                ++ "Parte de mi **código** está en [Github][github]. "
+                ++ "Una página alberga [mis **videojuegos** viejos][games]. "
+                ++ "En [Greasy Fork][greasyfork] están mis **userscripts**. "
+                ++ "En mi [Tumblr][tumblr] tengo cosas raras de la web."
+            )
     }
