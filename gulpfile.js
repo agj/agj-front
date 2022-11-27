29af8705311562f252880fabd6eaf25b596ba7f0
agj <ale@agj.cl>
2022-11-27 16:29:30 -0300

    Update gulpfile to ES module

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 
-const gulp = require('gulp');
-const elm = require('gulp-elm');
-const rename = require('gulp-rename');
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+import gulp from 'gulp'
+import elm from 'gulp-elm'
+import rename from 'gulp-rename'
+import { promisify } from 'util';
+import { exec as execOriginal } from 'child_process';
 
+const exec = promisify(execOriginal);
 
-const copy = () =>
+export const copy = () =>
 	gulp.src('source/copy/**')
 	.pipe(gulp.dest('build/'));
 
-const formatElm = () =>
+export const formatElm = () =>
 	exec('npx elm-format source/elm/ --yes');
 
-const buildElm = () =>
+export const buildElm = () =>
 	gulp.src('source/elm/Main.elm')
 	.pipe(elm({ optimize: true }))
 	.pipe(rename('main.js'))
@@ -25,17 +26,8 @@ const watchCopy = () =>
 const watchElm = () =>
 	gulp.watch('source/elm/**/*.elm', gulp.series(formatElm, buildElm));
 
-const build = gulp.parallel(copy, buildElm);
+export const build = gulp.parallel(copy, buildElm);
 
-const watch = gulp.parallel(watchCopy, watchElm);
+export const watch = gulp.parallel(watchCopy, watchElm);
 
-
-module.exports = {
-	default: build,
-	copy,
-	formatElm,
-	buildElm,
-	build,
-	watch,
-};
-
+export default build;
