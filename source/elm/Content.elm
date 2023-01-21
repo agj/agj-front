91b09da48a0c8511daa242e94f45e42e537ade26
agj <ale@agj.cl>
2023-01-21 20:13:03 -0300

    Refactor to deindent text

--- source/elm/Content.elm
+++ source/elm/Content.elm
@@ -43,24 +43,24 @@ mandarin : Content msg
 mandarin =
     { title = "艾磊（Ale Grilli）的個人網站"
     , intro =
-        intro
-            ("歡迎來到**艾磊**（Ale Grilli，**agj**）的網站。"
-                ++ "我是住在智利的「跨媒體創作人」。"
-                ++ "我會從事互動式媒體程式設計、視覺設計、影片拍攝等。"
-                ++ "我對語言非常狂熱。"
-                ++ "你想聯絡我的話，請把信寄到“ale¶agj.cl”（¶→@）。"
-            )
+        ("歡迎來到**艾磊**（Ale Grilli，**agj**）的網站。"
+            ++ "我是住在智利的「跨媒體創作人」。"
+            ++ "我會從事互動式媒體程式設計、視覺設計、影片拍攝等。"
+            ++ "我對語言非常狂熱。"
+            ++ "你想聯絡我的話，請把信寄到“ale¶agj.cl”（¶→@）。"
+        )
+            |> intro
     , menu = menu "作品集" "部落格"
     , links =
-        links
-            ("此外，我還有[**圖像日記**][piclog]。"
-                ++ "社交媒體裡面，雖然不常，我有時候會用 [Mastodon][mastodon]。"
-                ++ "我把做的**影片**放在 [Vimeo][vimeo] 或 [Youtube][youtube] 上。"
-                ++ "我一部分**程式碼**在 [Github][github] 上。"
-                ++ "在這邊你可以找到我很久以前做過的[**遊戲**][games]。"
-                ++ "我寫過的**使用者腳本**都在 [Greasy Fork][greasyfork] 裡面。"
-                ++ "曾經在網路上找到的一些東西還在我目前不理的 [Tumblr][tumblr] 裡。"
-            )
+        ("此外，我還有[**圖像日記**][piclog]。"
+            ++ "社交媒體裡面，雖然不常，我有時候會用 [Mastodon][mastodon]。"
+            ++ "我把做的**影片**放在 [Vimeo][vimeo] 或 [Youtube][youtube] 上。"
+            ++ "我一部分**程式碼**在 [Github][github] 上。"
+            ++ "在這邊你可以找到我很久以前做過的[**遊戲**][games]。"
+            ++ "我寫過的**使用者腳本**都在 [Greasy Fork][greasyfork] 裡面。"
+            ++ "曾經在網路上找到的一些東西還在我目前不理的 [Tumblr][tumblr] 裡。"
+        )
+            |> links
     }
 
 
@@ -68,24 +68,24 @@ japanese : Content msg
 japanese =
     { title = "アレ・グリリのウェブ"
     , intro =
-        intro
-            ("ようこそ、**アレ・グリリ**（Ale Grilli、別名 **agj**）のホームページへ。"
-                ++ "チリを拠点にする「メディア・クリエイター」です。"
-                ++ "インタラクティブ・メディアのプログラミングや、視覚デザイン、映像制作などに手掛ける者です。"
-                ++ "さらに言えば言語オタクです。"
-                ++ "ご連絡は “ale¶agj.cl”（¶→@）までお願いします。"
-            )
+        ("ようこそ、**アレ・グリリ**（Ale Grilli、別名 **agj**）のホームページへ。"
+            ++ "チリを拠点にする「メディア・クリエイター」です。"
+            ++ "インタラクティブ・メディアのプログラミングや、視覚デザイン、映像制作などに手掛ける者です。"
+            ++ "さらに言えば言語オタクです。"
+            ++ "ご連絡は “ale¶agj.cl”（¶→@）までお願いします。"
+        )
+            |> intro
     , menu = menu "作品集" "ブログ"
     , links =
-        links
-            ("他に、[**画像ログ**][piclog]を持っています。"
-                ++ "SNSなら、[マストドン][mastodon]にたまに現れます。"
-                ++ "手掛けた**映像作品**が [Vimeo][vimeo]、[Youtube][youtube] に。"
-                ++ "書いた**コード**の一部は [Github][github] 上に。"
-                ++ "昔作った[**ゲーム**][games]が置いてあるページもあります。"
-                ++ "[Greasy Fork][greasyfork] にたまに作るブラウザー用**ユーザースクリプト**があります。"
-                ++ "ウェブでの拾い物は [Tumblr][tumblr] に。"
-            )
+        ("他に、[**画像ログ**][piclog]を持っています。"
+            ++ "SNSなら、[マストドン][mastodon]にたまに現れます。"
+            ++ "手掛けた**映像作品**が [Vimeo][vimeo]、[Youtube][youtube] に。"
+            ++ "書いた**コード**の一部は [Github][github] 上に。"
+            ++ "昔作った[**ゲーム**][games]が置いてあるページもあります。"
+            ++ "[Greasy Fork][greasyfork] にたまに作るブラウザー用**ユーザースクリプト**があります。"
+            ++ "ウェブでの拾い物は [Tumblr][tumblr] に。"
+        )
+            |> links
     }
 
 
@@ -93,23 +93,23 @@ english : Content msg
 english =
     { title = "Ale Grilli’s website"
     , intro =
-        intro
-            ("Hello. This is my, **Ale Grilli** (a.k.a. **agj**)’s home on the web. "
-                ++ "I’m a Chile-based «cross-media creator»\u{200B}—\u{200B}"
-                ++ "I program interactive media, design visuals, author videos, and more. "
-                ++ "I'm also a languages nerd. You may contact me via “ale¶agj.cl” (¶→@)."
-            )
+        ("Hello. This is my, **Ale Grilli** (a.k.a. **agj**)’s home on the web. "
+            ++ "I’m a Chile-based «cross-media creator»\u{200B}—\u{200B}"
+            ++ "I program interactive media, design visuals, author videos, and more. "
+            ++ "I'm also a languages nerd. You may contact me via “ale¶agj.cl” (¶→@)."
+        )
+            |> intro
     , menu = menu "Portfolio" "Blog"
     , links =
-        links
-            ("I also have a [**pictures log**][piclog]. "
-                ++ "Socials-wise, I use [Mastodon][mastodon], although barely. "
-                ++ "My **video works** are on [Vimeo][vimeo] and [Youtube][youtube]. "
-                ++ "Some of my **code** is up on [Github][github]. "
-                ++ "I have a page with [my old **games**][games] in it. "
-                ++ "[Greasy Fork][greasyfork] houses my **userscripts**. "
-                ++ "My [Tumblr][tumblr] has some stuff I've found around the web."
-            )
+        ("I also have a [**pictures log**][piclog]. "
+            ++ "Socials-wise, I use [Mastodon][mastodon], although barely. "
+            ++ "My **video works** are on [Vimeo][vimeo] and [Youtube][youtube]. "
+            ++ "Some of my **code** is up on [Github][github]. "
+            ++ "I have a page with [my old **games**][games] in it. "
+            ++ "[Greasy Fork][greasyfork] houses my **userscripts**. "
+            ++ "My [Tumblr][tumblr] has some stuff I've found around the web."
+        )
+            |> links
     }
 
 
@@ -117,24 +117,24 @@ spanish : Content msg
 spanish =
     { title = "Web de Ale Grilli"
     , intro =
-        intro
-            ("Hola. Este es el sitio web de **Ale Grilli** (a veces **agj**). "
-                ++ "Soy un «creador transmedial»\u{200B}—\u{200B}"
-                ++ "desde Chile programo medios interactivos, diseño gráfica, creo videos, y otras cosas. "
-                ++ "Me gustan mucho los idiomas. "
-                ++ "Me puedes contactar por medio de “ale¶agj.cl” (¶→@)."
-            )
+        ("Hola. Este es el sitio web de **Ale Grilli** (a veces **agj**). "
+            ++ "Soy un «creador transmedial»\u{200B}—\u{200B}"
+            ++ "desde Chile programo medios interactivos, diseño gráfica, creo videos, y otras cosas. "
+            ++ "Me gustan mucho los idiomas. "
+            ++ "Me puedes contactar por medio de “ale¶agj.cl” (¶→@)."
+        )
+            |> intro
     , menu = menu "Portafolio" "Blog"
     , links =
-        links
-            ("También tengo un [**archivo de imágenes**][piclog]. "
-                ++ "En cuanto a redes sociales, uso [Mastodon][mastodon], a veces. "
-                ++ "Mis **videos** están en [Vimeo][vimeo] y [Youtube][youtube]. "
-                ++ "Parte de mi **código** está en [Github][github]. "
-                ++ "Una página alberga [mis **videojuegos** viejos][games]. "
-                ++ "En [Greasy Fork][greasyfork] están mis **userscripts**. "
-                ++ "En mi [Tumblr][tumblr] tengo cosas raras de la web."
-            )
+        ("También tengo un [**archivo de imágenes**][piclog]. "
+            ++ "En cuanto a redes sociales, uso [Mastodon][mastodon], a veces. "
+            ++ "Mis **videos** están en [Vimeo][vimeo] y [Youtube][youtube]. "
+            ++ "Parte de mi **código** está en [Github][github]. "
+            ++ "Una página alberga [mis **videojuegos** viejos][games]. "
+            ++ "En [Greasy Fork][greasyfork] están mis **userscripts**. "
+            ++ "En mi [Tumblr][tumblr] tengo cosas raras de la web."
+        )
+            |> links
     }
 
 
